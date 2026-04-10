4e321d01e017c12eed99c833df6e8c6e54f0799c
Mmegwa Godwin <charlesejike9111@gmail.com>
2026-04-10 13:51:20 +0100

    Update extensions.py
    Signed-off-by: Mmegwa Godwin  <charlesejike9111@gmail.com>

--- extensions.py
+++ extensions.py
@@ -1,7 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager # pyright: ignore[reportMissingImports]
 from flask_wtf.csrf import CSRFProtect # type: ignore
-
+ 
 db = SQLAlchemy()
 login_manager = LoginManager()
 csrf = CSRFProtect() 
